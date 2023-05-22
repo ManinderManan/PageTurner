f3578baced809724ca580020577cc833bb2db1c8
ManinderManan <manindersmanan@gmail.com>
2023-05-22 13:58:04 -0700

    updated layout

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,14 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  'pages_db',
-  'root',
-  'password',
-  {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    port: 3306
-  }
-);
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize('pages_db', 'root', 'password', {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
 module.exports = sequelize;
