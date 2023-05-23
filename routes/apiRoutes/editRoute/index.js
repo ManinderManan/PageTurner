71f949451a33d5f59751723d438ade5bd1695fa4
ManinderManan <123062785+ManinderManan@users.noreply.github.com>
2023-05-22 17:04:45 -0700

    Revert "dashboard"

--- routes/apiRoutes/editRoute/index.js
+++ routes/apiRoutes/editRoute/index.js
@@ -1,22 +1,17 @@
-const router = require('express').Router();
-const { Post, User } = require('../../../models');
+router.post('/', async (req,res)=> {
+         console.log(req.body);
 
-router.put('/:id', async (req,res)=> {
-    //console.log(req.body);
-    // localhost3001/api/post/1
     try {
-        const postData = await Post.update(req.body, {
-            where: {
-                id: req.params.id,
-            },
+        console.log(req.session);
+        const postData = await Post.create({...req.body,
+        user_id:req.session.user_id,
+        
         });
-        if(!postData){
-            res.status(404).json({message: 'No post found with this id!'});
-            return;
-        }
         res.json(postData);
+    
+    // res.send('hello');
     } catch (err) {
+        console.log(err);
         res.status(500).json(err);
     }
 });
-module.exports = router;
